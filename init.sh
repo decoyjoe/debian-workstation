90067c50d016d36c93f5c92c998d11b682e1cde0
Joseph Larionov <decoyjoe@gmail.com>
2024-05-22 18:17:26 -0700

    Add support for separate CLI and Desktop environments

--- init.sh
+++ init.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
 
-script_root="$(realpath "${0}" | xargs dirname)"
+script_root="$(realpath "$(dirname "$0")")"
+
+# Install necessary collections and roles from the requirements file
 ansible-galaxy install -r "$script_root/requirements.yml" --force
+
+vars_file="$script_root/vars.yml"
+
+if [[ ! -f "$vars_file" ]]; then
+    echo "Select the Environment for the workstation (enter the number):"
+    options=("Desktop" "CLI")
+    select env_option in "${options[@]}"; do
+        case $REPLY in
+            1) workstation_environment="Desktop"; break;;
+            2) workstation_environment="CLI"; break;;
+            *) echo "Invalid option, please choose a number from the list."; continue;;
+        esac
+    done
+
+    cat << EOF > "$vars_file"
+---
+workstation_environment: $workstation_environment
+EOF
+
+    echo "Created $vars_file with workstation_environment set to $workstation_environment."
+fi
