b2bab58fd1e15b51e458a8a934f8313378a7b4db
Joseph Larionov <decoyjoe@gmail.com>
2024-05-24 16:48:56 -0700

    Move the bootstrap into a function so it can reload PATH in the same shell

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,56 +1,58 @@
 #!/bin/bash
 
-echo "Installing git and pipx..."
-sudo apt update && sudo apt install -y git pipx
-if [ $? -ne 0 ]; then
-    echo "Failed to install packages."
-    exit 1
-fi
+bootstrap() {
+  echo "Installing git and pipx..."
+  sudo apt update && sudo apt install -y git pipx
+  if [ $? -ne 0 ]; then
+      echo "Failed to install packages."
+      exit 1
+  fi
 
-echo ""
-echo "Creating build directory..."
-mkdir -p ~/build
-if [ $? -ne 0 ]; then
-    echo "Failed to create build directory."
-    exit 1
-fi
+  echo ""
+  echo "Creating build directory..."
+  mkdir -p ~/build
+  if [ $? -ne 0 ]; then
+      echo "Failed to create build directory."
+      exit 1
+  fi
 
-cd ~/build
+  cd ~/build
 
-echo ""
-echo "Cloning the debian-workstation repository..."
-git clone https://github.com/decoyjoe/debian-workstation.git
-if [ $? -ne 0 ]; then
-    echo "Failed to clone repository."
-    exit 1
-fi
+  echo ""
+  echo "Cloning the debian-workstation repository..."
+  git clone https://github.com/decoyjoe/debian-workstation.git
+  if [ $? -ne 0 ]; then
+      echo "Failed to clone repository."
+      exit 1
+  fi
 
-cd debian-workstation
+  cd debian-workstation
 
-echo ""
-echo "Setting up pipx and installing poetry..."
-pipx ensurepath
-if [ $? -ne 0 ]; then
-    echo "Failed to set up pipx path."
-    exit 1
-fi
+  echo ""
+  echo "Setting up pipx and installing poetry..."
+  pipx ensurepath
+  if [ $? -ne 0 ]; then
+      echo "Failed to set up pipx path."
+      exit 1
+  fi
 
-pipx install poetry
-if [ $? -ne 0 ]; then
-    echo "Failed to install poetry."
-    exit 1
-fi
+  pipx install poetry
+  if [ $? -ne 0 ]; then
+      echo "Failed to install poetry."
+      exit 1
+  fi
 
-echo ""
-echo "Sourcing .bash_profile to make poetry available..."
-if [ -f ~/.bash_profile ]; then
-    source ~/.bash_profile
-else
-    echo "Warning: .bash_profile not found. If pipx binaries are not available, please open a new terminal or source the appropriate profile file manually."
-fi
+  echo ""
+  echo "Sourcing .bash_profile to make poetry available..."
+  if [ -f ~/.bash_profile ]; then
+      source ~/.bash_profile
+  else
+      echo "Warning: .bash_profile not found. If pipx binaries are not available, please open a new terminal or source the appropriate profile file manually."
+  fi
 
-echo ""
-echo "Bootstrap complete."
-echo ""
-echo "Run $(realpath ~/build/debian-workstation/init.sh) to initialize the automation."
-echo ""
+  echo ""
+  echo "Bootstrap complete."
+  echo ""
+  echo "Run $(realpath ~/build/debian-workstation/init.sh) to initialize the automation."
+  echo ""
+}
